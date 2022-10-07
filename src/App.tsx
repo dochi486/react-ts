89eace3a3d95edc1685b689208790b74bf2e92e8
dochi486 <93mskim@gmail.com>
2022-10-07 23:20:42 +0900

    contextAPI 사용하도록 변경 중

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,21 @@ import NewTodo from './components/NewTodo';
 import { useState } from 'react';
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  // const [todos, setTodos] = useState<Todo[]>([]);
 
-  const addTodoHandler = (todoText: string) => {
-    const newTodo = new Todo(todoText);
+  // const addTodoHandler = (todoText: string) => {
+  //   const newTodo = new Todo(todoText);
 
-    setTodos((prevTodos) => {
-      return prevTodos.concat(newTodo);
-    });
-  };
+  //   setTodos((prevTodos) => {
+  //     return prevTodos.concat(newTodo);
+  //   });
+  // };
 
-  const removeTodoHandler = (todoId: string) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((todo) => todo.id !== todoId);
-    });
-  };
+  // const removeTodoHandler = (todoId: string) => {
+  //   setTodos((prevTodos) => {
+  //     return prevTodos.filter((todo) => todo.id !== todoId);
+  //   });
+  // };
 
   return (
     <div>
